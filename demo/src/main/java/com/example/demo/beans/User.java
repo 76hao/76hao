fc0f3a549313bd46bed01d20357381b1507ea905
An_J1ng <15039951250@139.com>
2020-01-14 07:09:10 +0800

    project

---
+++ demo/src/main/java/com/example/demo/beans/User.java
@@ -0,0 +1,29 @@
+package com.example.demo.beans;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author An_J1ng
+ * @Date 2020/1/14 -6:48
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User {
+    /**
+    *
+    */
+    private Integer id;
+    /**
+    *
+    */
+    private String name;
+    /**
+    *
+    */
+    private String addr;
+
+
+}
---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import com.example.demo.beans.User;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author An_J1ng
+ * @Date 2020/1/14 -6:48
+ */
+@RestController
+public class UserController {
+    @RequestMapping("/get/{id}")
+    public User t1(@PathVariable("id")Integer id){
+        User user = new User();
+        user.setId(id);
+        return user;
+    }
+}
